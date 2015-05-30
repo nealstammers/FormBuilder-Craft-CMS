ee45855d882412e2928a658311c378a58e50e422
owldesign <vadim@owl-design.net>
2015-05-30 00:33:31 -0700

    latest

--- controllers/FormBuilder_FormsController.php
+++ controllers/FormBuilder_FormsController.php
@@ -10,6 +10,7 @@ class FormBuilder_FormsController extends BaseController
 	{	
 		$variables['forms'] = craft()->formBuilder_forms->getAllForms();
 		$variables['tabs'] = $this->_getTabs();
+		$variables['settings'] = craft()->plugins->getPlugin('formbuilder');
 
 		return $this->renderTemplate('formbuilder/forms', $variables);
 	}
